70df4e9b36e99459f3346a08167bb4075259f904
ElemkayZ <272538@student.pwr.edu.pl>
2024-06-11 08:29:10 +0200

    skoczony minimax alpha beta

--- projekt3/main.cpp
+++ projekt3/main.cpp
@@ -2,183 +2,38 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
+#include "ticTacToe.cpp"
 using namespace std;
 
-enum class CellState { Empty, X, O };
 
-class TicTacToe {
-private:
-    vector<vector<CellState>> board;
-    int size;
-    int moves;
 
-public:
-    TicTacToe(int boardSize) : size(boardSize), moves(0) {
-        // Initialize the board with empty cells
-        board.resize(size, vector<CellState>(size, CellState::Empty));
-    }
-
-    void printBoard() {
-        // Print the current state of the board
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                char symbol;
-                switch (board[i][j]) {
-                    case CellState::X: symbol = 'X'; break;
-                    case CellState::O: symbol = 'O'; break;
-                    default: symbol = '.'; break;
-                }
-                cout << symbol << " ";
-            }
-            cout << endl;
-        }
-    }
-
-    bool makeMove(int row, int col, CellState player) {
-        // Make a move if the cell is empty
-        if (board[row][col] == CellState::Empty) {
-            board[row][col] = player;
-            moves++;
-            return true;
-        }
-        return false;
-    }
-
-    bool checkWin(CellState player) {
-        // Check rows and columns
-        for (int i = 0; i < size; ++i) {
-            if (checkRow(i, player) || checkCol(i, player)) return true;
-        }
-        // Check diagonals
-        return checkDiagonal(player) || checkAntiDiagonal(player);
-    }
-
-    bool checkRow(int row, CellState player) {
-        for (int j = 0; j < size; ++j) {
-            if (board[row][j] != player) return false;
-        }
-        return true;
-    }
-
-    bool checkCol(int col, CellState player) {
-        for (int i = 0; i < size; ++i) {
-            if (board[i][col] != player) return false;
-        }
-        return true;
-    }
-
-    bool checkDiagonal(CellState player) {
-        for (int i = 0; i < size; ++i) {
-            if (board[i][i] != player) return false;
-        }
-        return true;
-    }
-
-    bool checkAntiDiagonal(CellState player) {
-        for (int i = 0; i < size; ++i) {
-            if (board[i][size - 1 - i] != player) return false;
-        }
-        return true;
-    }
-
-    bool isBoardFull() {
-        return moves == size * size;
-    }
-
-    pair<int, int> getBestMove() {
-        int bestVal = numeric_limits<int>::min();
-        pair<int, int> bestMove = {-1, -1};
-
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                if (board[i][j] == CellState::Empty) {
-                    board[i][j] = CellState::O;
-                    int moveVal = minimax(0, false);
-                    board[i][j] = CellState::Empty;
-
-                    if (moveVal > bestVal) {
-                        bestMove = {i, j};
-                        bestVal = moveVal;
-                    }
-                }
-            }
-        }
-
-        return bestMove;
-    }
-
-    int minimax(int depth, bool isMax) {
-        if (checkWin(CellState::O)) return 10 - depth;
-        if (checkWin(CellState::X)) return depth - 10;
-        if (isBoardFull()) return 0;
-
-        if (isMax) {
-            int best = numeric_limits<int>::min();
-            for (int i = 0; i < size; ++i) {
-                for (int j = 0; j < size; ++j) {
-                    if (board[i][j] == CellState::Empty) {
-                        board[i][j] = CellState::O;
-                        best = max(best, minimax(depth + 1, !isMax));
-                        board[i][j] = CellState::Empty;
-                    }
-                }
-            }
-            return best;
-        } else {
-            int best = numeric_limits<int>::max();
-            for (int i = 0; i < size; ++i) {
-                for (int j = 0; j < size; ++j) {
-                    if (board[i][j] == CellState::Empty) {
-                        board[i][j] = CellState::X;
-                        best = min(best, minimax(depth + 1, !isMax));
-                        board[i][j] = CellState::Empty;
-                    }
-                }
-            }
-            return best;
-        }
-    }
-};
 
 int main() {
+    bool playing = true;
     int boardSize;
+    char playerMark;
+    int toWin;
     cout << "Enter the size of the Tic Tac Toe board: ";
     cin >> boardSize;
-
-    TicTacToe game(boardSize);
-
-    while (true) {
-        // Player X's turn
-        int x, y;
-        cout << "Player X's turn. Enter row and column (0-indexed): ";
-        cin >> x >> y;
-        if (!game.makeMove(x, y, CellState::X)) {
-            cout << "Invalid move. Try again.\n";
-            continue;
-        }
-        game.printBoard();
-        if (game.checkWin(CellState::X)) {
-            cout << "Player X wins!\n";
-            break;
-        }
-        if (game.isBoardFull()) {
-            cout << "It's a draw!\n";
-            break;
-        }
-
-        // Player O's turn using Minimax
-        cout << "Player O's turn.\n";
-        pair<int, int> bestMove = game.getBestMove();
-        game.makeMove(bestMove.first, bestMove.second, CellState::O);
-        game.printBoard();
-        if (game.checkWin(CellState::O)) {
-            cout << "Player O wins!\n";
-            break;
-        }
-        if (game.isBoardFull()) {
-            cout << "It's a draw!\n";
-            break;
-        }
+    cout << "Pick X or O: ";
+    cin >> playerMark;
+    cout << "How many in a line to win: ";
+    cin >> toWin;
+    TicTacToe game(boardSize,playerMark,toWin);
+    
+    while (playing) {
+        // system("cls");
+        // game.printBoard();
+        if(game.playerMark == CellState::X && playing == true)
+            playing = game.player(&game);
+            // playing = game.bot2(&game);
+        if(playing == true)
+            playing = game.bot(&game);
+        if(game.playerMark != CellState::X && playing == true)
+            playing = game.player(&game);
+            // playing = game.bot2(&game);
+        
+        
     }
 
     return 0;
---
+++ projekt3/ticTacToe.cpp
@@ -0,0 +1,260 @@
+#include "ticTacToe.h"
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+using namespace std;
+
+    void TicTacToe::printBoard() {
+        // Print the current state of the board
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                char symbol;
+                switch (board[i][j]) {
+                    case CellState::X: symbol = 'X'; break;
+                    case CellState::O: symbol = 'O'; break;
+                    default: symbol = '.'; break;
+                }
+                cout << symbol << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    bool TicTacToe::makeMove(int row, int col, CellState player) {
+        // Make a move if the cell is empty
+        if (board[row][col] == CellState::Empty) {
+            board[row][col] = player;
+            moves++;
+            return true;
+        }
+        return false;
+    }
+
+    int TicTacToe::checkWin() {
+        // Check rows
+        for (int i = 0; i < size; ++i) {
+            int playerCount = 0, botCount = 0;
+            for (int j = 0; j < size; ++j) {
+                if (board[i][j] == playerMark) {
+                    playerCount++;
+                    botCount = 0;
+                } else if (board[i][j] == botMark) {
+                    botCount++;
+                    playerCount = 0;
+                } else {
+                    playerCount = 0;
+                    botCount = 0;
+                }
+                if (playerCount == toWin) return 1;
+                if (botCount == toWin) return -1;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < size; ++j) {
+            int playerCount = 0, botCount = 0;
+            for (int i = 0; i < size; ++i) {
+                if (board[i][j] == playerMark) {
+                    playerCount++;
+                    botCount = 0;
+                } else if (board[i][j] == botMark) {
+                    botCount++;
+                    playerCount = 0;
+                } else {
+                    playerCount = 0;
+                    botCount = 0;
+                }
+                if (playerCount == toWin) return 1;
+                if (botCount == toWin) return -1;
+            }
+        }
+
+        // Check main diagonals
+        for (int i = 0; i <= size - toWin; ++i) {
+            for (int j = 0; j <= size - toWin; ++j) {
+                int playerCount = 0, botCount = 0;
+                for (int k = 0; k < toWin; ++k) {
+                    if (board[i + k][j + k] == playerMark) {
+                        playerCount++;
+                        botCount = 0;
+                    } else if (board[i + k][j + k] == botMark) {
+                        botCount++;
+                        playerCount = 0;
+                    } else {
+                        playerCount = 0;
+                        botCount = 0;
+                    }
+                    if (playerCount == toWin) return 1;
+                    if (botCount == toWin) return -1;
+                }
+            }
+        }
+
+        // Check anti-diagonals
+        for (int i = 0; i <= size - toWin; ++i) {
+            for (int j = toWin - 1; j < size; ++j) {
+                int playerCount = 0, botCount = 0;
+                for (int k = 0; k < toWin; ++k) {
+                    if (board[i + k][j - k] == playerMark) {
+                        playerCount++;
+                        botCount = 0;
+                    } else if (board[i + k][j - k] == botMark) {
+                        botCount++;
+                        playerCount = 0;
+                    } else {
+                        playerCount = 0;
+                        botCount = 0;
+                    }
+                    if (playerCount == toWin) return 1;
+                    if (botCount == toWin) return -1;
+                }
+            }
+        }
+
+        return 0;
+    }   
+
+
+    bool TicTacToe::isBoardFull() {
+        return moves == size * size;
+    }
+
+    pair<int, int> TicTacToe::getBestMove() {
+        int current;
+        int best = numeric_limits<int>::max();
+        pair<int, int> currentPoint = {-1, -1};
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (board[i][j] == CellState::Empty) {
+                    board[i][j] = botMark;
+                    int current = minimax(MAX_DEPTH,-best,best, true);
+                    board[i][j] = CellState::Empty;
+                    if (current < best) {
+                        currentPoint = {i, j};
+                        best = current;
+                    }
+                }
+            }
+        }
+
+        return currentPoint;
+    }
+
+    int TicTacToe::minimax(int depth, int alpha, int beta, bool isMax) {
+        int gameState = checkWin();
+        if (gameState != 0 || depth == 0) {
+        // if (gameState == 1) return 1 - depth;   // Player win
+        // if (gameState == -1) return depth - 1;  // Bot win
+        return gameState;
+        }
+        if(isBoardFull()){
+            return 0;
+        }
+
+        if (isMax) {
+            int max = -std::numeric_limits<int>::max();
+            for (int i = 0; i < size; ++i) {
+                for (int j = 0; j < size; ++j) {
+                    if (board[i][j] == CellState::Empty) {
+                        board[i][j] = playerMark;
+                        max = std::max(max,minimax(depth - 1, alpha, beta, false));
+                        board[i][j] = CellState::Empty;
+                        alpha = std::max(alpha, max);
+                        if (beta <= alpha)
+                            return alpha; // Alpha-beta pruning
+                    }
+                }
+            }
+            return max;
+        } 
+        else {
+            int min = std::numeric_limits<int>::max();
+            for (int i = 0; i < size; ++i) {
+                for (int j = 0; j < size; ++j) {
+                    if (board[i][j] == CellState::Empty) {
+                        board[i][j] = botMark;
+                        min = std::min(min,minimax(depth - 1, alpha, beta, true));
+                        board[i][j] = CellState::Empty;
+                        beta = std::min(beta, min);
+                        if (beta <= alpha)
+                            return beta; // Alpha-beta pruning
+                    }
+                }
+            }
+            return min;
+        }
+    }
+    bool TicTacToe::bot(TicTacToe*game){
+        if (game->checkWin()==-1) {
+            cout << "Bot wins!\n";
+            return false;
+        }
+        if (game->checkWin()==1) {
+            cout << "Player wins!\n";
+            return false;
+        }
+        if (game->isBoardFull()) {
+            cout << "It's a draw!\n";
+            return false;
+        }
+        cout << "Bot turn.\n";
+        pair<int, int> bestMove = game->getBestMove();
+        game->makeMove(bestMove.first, bestMove.second, CellState::O);
+        game->printBoard();
+        
+        
+        return true;
+    }
+    bool TicTacToe::bot2(TicTacToe*game){
+        if (game->checkWin()==-1) {
+            cout << "Bot wins!\n";
+            return false;
+        }
+        if (game->checkWin()==1) {
+            cout << "Bot2 wins!\n";
+            return false;
+        }
+        if (game->isBoardFull()) {
+            cout << "It's a draw!\n";
+            return false;
+        }
+        cout << "Bot2 turn.\n";
+        playerMark = CellState::O;
+        botMark = CellState::X;
+        pair<int, int> bestMove = game->getBestMove();
+        game->makeMove(bestMove.first, bestMove.second, CellState::X);
+        playerMark = CellState::X;
+        botMark = CellState::O;
+        game->printBoard();
+        
+        
+        return true;
+    }
+
+    bool TicTacToe::player(TicTacToe*game){
+        
+        if (game->checkWin()==-1) {
+            cout << "Bot wins!\n";
+            return false;
+        }
+        if (game->checkWin()==1) {
+            cout << "Player wins!\n";
+            return false;
+        }
+        if (game->isBoardFull()) {
+            cout << "It's a draw!\n";
+            return false;
+        }
+        int x, y;
+        cout << "Players turn. Enter row and column (0-indexed): ";
+        cin >> x >> y;
+        if (!game->makeMove(x, y, CellState::X)) {
+            cout << "Invalid move. Try again.\n";
+            return true;
+        }
+        game->printBoard();
+        
+        
+        return true;
+    }
